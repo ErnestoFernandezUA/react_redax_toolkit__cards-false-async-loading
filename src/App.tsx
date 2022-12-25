e4649d0d72079d872bf414a19eb0ba7d87888a66
E Fernandez <ernesto.fernandez.1980@gmail.com>
2022-12-25 20:45:16 +0200

    first

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ export const App: React.FC = () => {
   return (
     <div className="starter">
       <Provider onClick={() => ({})}>
-        <TodoList />
+        TodoList
       </Provider>
     </div>
   );
